57e660a2f72248e359e8382481bcaa1e55f8e100
DoanThuong <doanthuong.jp@gmail.com>
2026-04-30 11:06:49 +0900

    Refactor Rook class to Queen with valid move logic

--- ChessGame_2D/src/com/chess/model/Rook.java
+++ ChessGame_2D/src/com/chess/model/Rook.java
@@ -1,17 +1,19 @@
 package com.chess.model;
 
-public class Rook extends Piece {
-    public Rook(int row, int col, boolean isWhite) {
-        super(row, col, isWhite, "rook");
+public class Queen extends Piece {
+    public Queen(int row, int col, boolean isWhite) {
+        super(row, col, isWhite, "queen");
     }
 
     @Override
     public boolean isValidMove(int targetRow, int targetCol, Piece[][] board) {
-        // Chỉ đi thẳng hoặc ngang
-        if (this.row == targetRow || this.col == targetCol) {
-            // Kiểm tra vật cản trên đường đi
+        // 斜め移動かどうか
+        boolean isDiagonal = Math.abs(targetRow - this.row) == Math.abs(targetCol - this.col);
+        // 縦または横の直線移動かどうか
+        boolean isStraight = (this.row == targetRow || this.col == targetCol);
+
+        if (isDiagonal || isStraight) {
             if (isPathClear(targetRow, targetCol, board)) {
-                // Kiểm tra ô đích: trống hoặc là quân đối phương
                 Piece target = board[targetRow][targetCol];
                 return target == null || target.isWhite != this.isWhite;
             }
